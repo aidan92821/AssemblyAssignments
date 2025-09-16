2bbc470c3e85262ab85736dfe4496ff32031f372
aidan92821 <aidan92821@gmail.com>
2025-09-16 16:45:01 -0700

    Refactor assembly program to correctly compute the sum of two 16-bit numbers and store it as a 32-bit result

--- Assignment2/add.asm
+++ Assignment2/add.asm
@@ -7,18 +7,29 @@
 ; }
 
 section .data
-    num1 dw  65244          ; Declare 16-bit variable num1
-    num2 dw  4660           ; Declare 16-bit variable num2
-    sum  dd  0              ; Declare 32-bit variable sum
-    SYS_exit equ 60
-    EXIT_SUCCESS equ 0
+    SYS_exit        equ 60
+    EXIT_SUCCESS    equ 0
+    num1            dw  65244            ; Declare 16-bit variable
+    num2            dw  4660             ; Declare 16-bit variable
+    sum             dd  0                ; Declare 32-bit variable
 
 section .text
     global _start
 _start:
-    mov ax, word[num1]      ; Load num1 into AX (16-bit)
-    add ax, word[num2]      ; Add num2 to AX (16-bit)
-    mov word[sum], ax       ; Store the result in sum (32-bit)
-    mov rax, SYS_exit
-    mov rdi, EXIT_SUCCESS
+    mov     dx, 0               ; DX = high 16 bit sum start at 0
+
+    ; Low 16-bit add
+    mov     ax, word[num1]         ; AX = low 16 bit num1
+    add     ax, word[num2]          ; AX = low16 bit sum 
+
+    ; High 16 bits carry
+    adc     dx, 0               ; DX += CF 
+
+    ; Store 32-bit sum
+    mov     word[sum],   ax         ; low  16 bit
+    mov     word[sum+2], dx         ; high 16 bit
+
+    ; exit
+    mov     rax, SYS_exit
+    mov     rdi, EXIT_SUCCESS
     syscall
